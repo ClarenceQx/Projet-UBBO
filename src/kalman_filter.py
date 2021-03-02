a77e645b8711a61ef9f44b1d3c211b3d4ed261ca
Mathieu Deransart <mathieu.deransart@centrale-marseille.fr>
2021-03-02 13:55:09 +0100

    Update kalman_filter.py
    Filtre de Kalman fonctionnel ?
    Il faut faire des vérifs + implémenter l'écriture dans les topics

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -1,4 +1,10 @@
 """
+Reste à faire :
+- Vérifier la jacobienne 'C' de l'odom
+- Transformer les coordonnées (x, y, theta) du lydar par scan2d en (x, y, theta) du centre du chassis
+dans la fonction 'callback_pos_scan2d' avec la tf
+- Gérer la sortie des donnéees -> mise en forme par tf et envoies dans les bons topics
+
 Prérequis :
 - Créer un message 'Scan2d' tel qu'on puisse accéder aux 'attributs' :
 Scan2d.x, Scan2d.y, Scan2d.theta
@@ -15,64 +21,50 @@ REMARQUE :
 -On s'embête à gérer les matrices sans Numpy, mais si le package peut être rajouter, ça simplifierai le code
 et accélèrerai peut-être significativement les calculs (peut important à l'initialisation, plus dans la boucle
 """
+
 import rospy
 import tf2_ros
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float32
 from std_msgs.msg import Header
 from math import cos, sin, pi
+import numpy as np
 
-# Fonctions matricielles
-def somme(A, B)
-    return [[A[i][j]+B[i][j] for j in range(len(A[0]))] for i in range(len(A))]
-
-def mul(A, B):
-    # On suppose les dimensions correctes ! donc len(A[0]) == len(B)
-    n = len(B)
-    return [[sum([A[i][k]*B[k][j] for k in range(n)])for j in range(len(B[0]))] for i in range(len(A))]
-
-def mul_scalaire(a, B):
-    return [[a*b for b in ligne]for ligne in B]
-
-def transpose(A):
-    return [[A[j][i] for j in range(len(A[0]))] for i in range(len(A))]
-
-def diagonal(ligne):
-    n = len(ligne)
-    res = [[0 for j in range(n)] for i in range(n)]
-    for i in range(n):
-        res[i][i] = ligne[i]
-    return res
 
 
 LARGEUR = 0.215  # Largeur du chassis
 RAYON = 0.05  # Rayon des roues
-EYE5 = diagonal([1, 1, 1, 1, 1])
-SIGMA_OMEGA =[  # Variance du Modèle, pris dans la littérature (cf Justin Cano)
+EYE5 = np.matrix([
+    [1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 0],
+    [0, 0, 1, 0, 0],
+    [0, 0, 0, 1, 0],
+    [0, 0, 0, 0, 1]
+])
+SIGMA_OMEGA =np.matrix([  # Variance du Modèle, pris dans la littérature (cf Justin Cano)
     [0.01**2, 0, 0, 0, 0],  # Confiance en x
     [0, 0.01**2, 0, 0, 0],  # Confiance en y
     [0, 0, 0.2**2, 0, 0],   # Confiance en vt
     [0, 0, 0, 0.01**2, 0],  # Confiance en theta
     [0, 0, 0, 0, 0.2**2]    # Confiance en omega
-]
+])
 doute_pos_initial = 2
-sigma = SIGMA_OMEGA.copy()
-sigma = mul_scalaire(doute_pos_initial, sigma)
+sigma = doute_pos_initial * SIGMA_OMEGA  # Initialisation pour les itérations du filtre de Kalman
 
 std_odometry = 0.01
 std_odometry_translation = std_odometry*RAYON/(2**.5)
 std_odometry_rotation = std_odometry*(2**.5)*RAYON/LARGEUR
-SIGMA_NU_ODOM = [  # Variance des acquisitions odométriques
+SIGMA_NU_ODOM = np.matrix([  # Variance des acquisitions odométriques
     [std_odometry_translation**2, 0],
     [0, std_odometry_rotation**2]
-]
+])
 std_position_map = 0.02
 std_theta_map = 5 * (pi/180)  # Imprécision en radians
-SIGMA_NU_SCAN2D = [  # Variance des acquisitions scan2d
+SIGMA_NU_SCAN2D = np.matrix([  # Variance des acquisitions scan2d
     [std_position_map**2, 0, 0],
     [0, std_position_map**2, 0],
     [0, 0, std_theta_map**2]
-]
+])
 
 pos_l = 0
 pos_r = 0
@@ -87,24 +79,28 @@ new_scan = False  # variable indicant à la boucle principale si les données du
 
 
 def callback_vit_r(msg):
+    """On veut la vitesse de déplacement (!= rotation) de la roue droite"""
     global vit_r
     vit_r = msg.data
     return ()
 
 
 def callback_vit_l(msg):
+    """On veut la vitesse de déplacement (!= rotation) de la roue gauche"""
     global vit_l
     vit_l = msg.data
     return ()
 
 
 def callback_cmd_r(msg):
+    """On veut la consigne de déplacement (!= rotation) de la roue droite"""
     global cmd_r
     cmd_r = msg.data
     return ()
 
 
 def callback_cmd_l(msg):
+    """On veut la consigne de déplacement (!= rotation) de la roue gauche"""
     global cmd_l
     cmd_l = msg.data
     return ()
@@ -112,10 +108,11 @@ def callback_cmd_l(msg):
 
 ################################## À modifier une fois que le message du scan2D sera codé si besoin ######################
 def callback_pos_scan_2d(msg):
+    """On veut les coordonnées (x, y, theta) pour le centre du chassis"""
     global pos_scan_2d
-    pos_scan_2d[0] = msg.x
-    pos_scan_2d[1] = msg.y
-    pos_scan_2d[2] = msg.theta
+    pos_scan_2d[0] = msg.x  # ############# Utiliser la TF pour passer de lydar -> centre du chassis
+    pos_scan_2d[1] = msg.y  # ############# Utiliser la TF pour passer de lydar -> centre du chassis
+    pos_scan_2d[2] = msg.theta  # Theta est sensé être le même pour tout le robot (?)
     global new_scan
     new_scan = True
 
@@ -126,7 +123,7 @@ rate = rospy.Rate(10)
 
 ## Définir le message à envoyer ici en l'initialisant (avec le topic /initialpose) ################################
 # X = [x, y, vt, theta, omega] -> pris sur la position centrale du Robot !
-X_i = [0 for i in range(5)]
+X_hat = np.matrix([0 for i in range(5)]).transpose()  # matrice colonne
 """
 Subscribe to "initialpose", et obtenir x, y, theta
 vt = omega = 0 (vitesses nulles à l'initialisation)
@@ -152,44 +149,43 @@ while not rospy.is_shutdown():
 
     # Étape 1 : PRÉDICTION
     # -> utilisation de la vitesse cible pour la prédiction
-    theta = X_i[3]
+    theta = X_hat[3]
     vt = (cmd_l + cmd_r) / 2
     omega = (cmd_l - cmd_r) / LARGEUR
     f = [vt*cos(theta), vt*sin(theta), 0, omega, 0]
-    X_pred = X_i.copy()
+    X_pred = X_hat.copy()
     X_pred[0] += vt * cos(theta) * dt
     X_pred[1] += vt * sin(theta) * dt
     X_pred[2] = vt
     X_pred[3] += omega * dt
     X_pred[4] = omega
     # Calcul de Ak ...
-    A = [
+    A = np.matrix([
         [0, 0, cos(theta), -vt*sin(theta), 0],
         [0, 0, sin(theta), vt*cos(theta), 0],
         [0, 0, 0, 0, 0],
         [0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0]
-    ]
-    Ak = somme(EYE5, mul_scalaire(dt, A))  # Ak = eye(5) + A*dt
-    sigma_pred = somme(SIGMA_OMEGA, mul(Ak, mul(sigma, transpose(Ak))))  # sigma_pred = Ak * sigma * transposée_de(Ak) + sigma_omega;
+    ])
+    Ak = EYE5 + A * dt
+    sigma_pred = SIGMA_OMEGA + Ak * sigma * (Ak.transpose())
 
     # Étape '3' : CHOIX DES MESURES
     if new_scan:  # on utilise le scan_2d
         new_scan = False
         # La mesure
-        y = pos_scan_2d.copy()
+        y = np.matrix(pos_scan_2d.copy()).transpose
         # Jacobienne
-        C = [
+        C = np.matrix([
             [1, 0, 0, 0, 0],
             [0, 1, 0, 0, 0],
             [0, 0, 0, 1, 0]
-        ]
-        g_x_pred = [X_pred[0], X_pred[1], X_pred[3]]
+        ])
+        g_x_pred = np.matrix([X_pred[0, 0], X_pred[1, 0], X_pred[3, 0]]).transpose()
         # innovation
-        innovation = [y[i] - g_x_pred[i] for i in range(3)]
+        innovation = y - g_x_pred
         # Variance des capteurs
         sigma_nu = SIGMA_NU_SCAN2D
-
 
     else:  # on utilise l'odométrie
         # La mesure
@@ -200,18 +196,21 @@ while not rospy.is_shutdown():
             [0, 0, 1, 0, -LARGEUR/2]
         ]
         # Mesure prédite
-        g_x_pred = [X_pred[2]+X_pred[4]*LARGEUR/2, X_pred[2]-X_pred[4]*LARGEUR/2]
+        g_x_pred = np.matrix([X_pred[2, 0]+X_pred[4, 0]*LARGEUR/2, X_pred[2, 0]-X_pred[4, 0]*LARGEUR/2]).transpose()
         # innovation
-        innovation = [y[i] - g_x_pred[i] for i in range(2)]
+        innovation = y - g_x_pred
         # Variance des capteurs
         sigma_nu = SIGMA_NU_ODOM
 
     # Étape '2' : GAIN DE KALMAN
-    G = 0.5
-    # G = sigma_pred * C'*(C*sigma_pred*C' + sigma_nu) ^ -1; PUTAIN D'INVERSION DE MATRICE
+    G = sigma_pred * (C.transpose()) * (C * sigma_pred * (C.transpose()) + sigma_nu)**-1
 
     # CALCULS FINAUX
-    X_i = [X_pred[i] + ]
-    # xhat(:, i) = xpred + G * innovation;
-    # sigma = (eye(5) - G * C) * sigma_pred;
+    X_hat = X_pred + G * innovation
+    sigma = (EYE5 - G * C) * sigma_pred
+
+
+    # TRANSMISSION DES RÉSULTATS
+    # On a construit notre estimée X_hat = matrix([x, y, vt, theta, omega]) du centre de notre chassis
+    # Il faut mettre en forme cette information et la transmettre dans les bons topics
 
